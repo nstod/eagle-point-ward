e754cbd7f8950a38742ffddbdd02fe02a9e0c773
Nate Stoddart <nstod11@gmail.com>
2023-01-22 16:21:18 -0700

    Update to Angular 11

--- src/app/components/dinners/dinners.component.spec.ts
+++ src/app/components/dinners/dinners.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { DinnersComponent } from './dinners.component';
 
@@ -6,7 +6,7 @@ describe('DinnersComponent', () => {
   let component: DinnersComponent;
   let fixture: ComponentFixture<DinnersComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ DinnersComponent ]
     })
